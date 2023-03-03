1e6b04f13823ca2ff2f1dda4128ec132ffc8c67f
Eric O'Callaghan <eric@ericoc.com>
2023-03-03 17:44:08 -0500

    demo qbittorrent api thing

--- __init__.py
+++ __init__.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python3
-'''pyplay'''
 
+import qbittorrentapi
 
-def main():
-    '''main()'''
-    try:
-        x = int(input("What's x? "))
-        print(x, "squared is", square(x))
-    except ValueError:
-        print("That's not a valid number!")
+# instantiate a Client using the appropriate WebUI configuration
+qbt_client = qbittorrentapi.Client(
+    host='localhost',
+    port=8080,
+    username='admin',
+    password='adminadmin',
+)
 
+# the Client will automatically acquire/maintain a logged-in state
+# in line with any request. therefore, this is not strictly necessary;
+# however, you may want to test the provided login credentials.
+try:
+    qbt_client.auth_log_in()
+except qbittorrentapi.LoginFailed as e:
+    print(e)
 
-def square(n):
-    '''square()'''
-    return n * n
+# display qBittorrent info
+print(f'qBittorrent: {qbt_client.app.version}')
+print(f'qBittorrent Web API: {qbt_client.app.web_api_version}')
+for k,v in qbt_client.app.build_info.items(): print(f'{k}: {v}')
 
-
-# run
-main()
+# retrieve and show all torrents
+for torrent in qbt_client.torrents_info():
+    print(f'{torrent.hash[-6:]}: {torrent.name} ({torrent.state})')
