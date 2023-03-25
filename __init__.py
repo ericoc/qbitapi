00818c7d20f733647fbe330ecab5416eb6841e2f
Eric O'Callaghan <eric@ericoc.com>
2023-03-25 17:45:58 -0400

    numerous updates...

--- __init__.py
+++ __init__.py
@@ -1,72 +1,41 @@
 #!/usr/bin/env python3
 '''qbitapi'''
+
 import logging
-from datetime import datetime, timezone
-from flask import abort, Flask, flash, g, redirect, render_template, url_for
-import pytz
+import os
+
+from flask import abort, Flask, flash, g, redirect, render_template, \
+    send_from_directory, url_for
 import qbittorrentapi
 
 
+# Configure logging
 logging.getLogger('qbittorrentapi').setLevel(logging.DEBUG)
 logging.getLogger('requests').setLevel(logging.DEBUG)
 logging.getLogger('urllib3').setLevel(logging.DEBUG)
 
+# Initialize Flask
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 
 @app.before_request
-def pre_request():
+def pre():
     '''Before request'''
-#    g.now = datetime.now(tz=timezone.utc)
-    g.qbit = qbc()
+    g.qbit = _qbc()
 
 
 @app.context_processor
 def injects():
     '''Variables available to all templates'''
-#    return { 'now': g.now }
     return {}
 
 
-def fmt_ratio(ratio=None):
-    '''Return CSS class for ratio'''
-    if ratio >= 1:
-        return 'done'
-    if ratio >= 0.5:
-        return 'high'
-    if ratio >= 0.25:
-        return 'low'
-    return 'verylow'
-
-
-def fmt_size(num_bytes=None):
-    '''Convert bytes to human-readable file size'''
-    if isinstance(num_bytes, int):
-        for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
-            if abs(num_bytes) < 1024.0:
-                return f'{num_bytes:3.1f} {unit}B'
-            num_bytes /= 1024.0
-        return f'{num_bytes:.1f} YiB'
-    return num_bytes
-
-
-def fmt_time(timestamp=None):
-    '''Convert UNIX timestamp to Python datetime object'''
-    if isinstance(timestamp, int):
-        return datetime.fromtimestamp(timestamp, tz=timezone.utc)
-    return timestamp
-
-
-def fmt_tz(when=None):
-    '''Convert Python datetime object to configured timezone'''
-    if isinstance(when, datetime):
-        return when.astimezone(pytz.timezone(app.config['TIMEZONE']))
-    return when
-
-
-def qbc(host=app.config['QBIT_HOST'], port=app.config['QBIT_PORT'],
-    username=app.config['QBIT_USERNAME'], password=app.config['QBIT_PASSWORD']):
+def _qbc(
+    host=app.config['QBIT_HOST'], port=app.config['QBIT_PORT'],
+    username=app.config['QBIT_USERNAME'],
+    password=app.config['QBIT_PASSWORD']
+):
     '''Connect and log in to qBittorrent API'''
     client = qbittorrentapi.Client(
         host=host, port=port,
@@ -86,7 +55,7 @@ def index(torrents=None):
     if not torrents:
         flash('Sorry, but no torrents could be found!', 'error')
         abort(404)
-    return render_template('index.html.j2', torrents=torrents, qbit_version=g.qbit.app_version())
+    return render_template('index.html.j2', torrents=torrents)
 
 
 @app.route('/category/<path:category>/', methods=['GET'])
@@ -98,29 +67,26 @@ def search_category(category=None):
     return index(torrents=g.qbit.torrents_info(category=category))
 
 
-def error(title='Unknown Error', message='Sorry, but there was an unknown error.', code=500):
+def error(
+        title='Unknown Error',
+        message='Sorry, but there was an unknown error.',
+        code=500
+):
     '''Error template'''
     return render_template('error.html.j2', title=title, message=message), code
+
 
 @app.errorhandler(400)
 def bad_request(message):
     '''Error codes to template above'''
     return error(title='Bad Request', message=message, code=400)
 
-@app.errorhandler(401)
-def not_authorized(message):
-    '''Error codes to template above'''
-    return error(title='Not Authorized', message=message, code=401)
-
-@app.errorhandler(403)
-def forbidden(message):
-    '''Error codes to template above'''
-    return error(title='Forbidden', message=message, code=403)
 
 @app.errorhandler(404)
 def page_not_found(message):
     '''Error codes to template above'''
     return error(title='Page Not Found', message=message, code=404)
+
 
 @app.errorhandler(500)
 def internal_server_error(message):
@@ -128,5 +94,14 @@ def internal_server_error(message):
     return error(title='Internal Server Error', message=message, code=500)
 
 
+@app.route('/favicon.ico')
+def favicon():
+    '''Serve favicon.ico from static content'''
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+        'favicon.ico', mimetype='image/vnd.microsoft.icon'
+    )
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
