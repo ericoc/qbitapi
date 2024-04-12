f1d242bb257f796088dfc57d29bdb6281b5f84b7
Eric O'Callaghan <eric@ericoc.com>
2024-04-12 10:02:19 -0400

    datatables + bootstrap

--- __init__.py
+++ __init__.py
@@ -1,107 +1,113 @@
 #!/usr/bin/env python3
-'''qbitapi'''
+"""qbitapi."""
 
 import logging
 import os
 
-from flask import abort, Flask, flash, g, redirect, render_template, \
-    send_from_directory, url_for
+from flask import abort, Flask, flash, g, redirect, render_template, url_for, \
+    send_from_directory
 import qbittorrentapi
 
 
-# Configure logging
-logging.getLogger('qbittorrentapi').setLevel(logging.DEBUG)
-logging.getLogger('requests').setLevel(logging.DEBUG)
-logging.getLogger('urllib3').setLevel(logging.DEBUG)
+# Logging.
+logging.getLogger("qbittorrentapi").setLevel(logging.DEBUG)
+logging.getLogger("requests").setLevel(logging.DEBUG)
+logging.getLogger("urllib3").setLevel(logging.DEBUG)
 
-# Initialize Flask
+# Flask.
 app = Flask(__name__)
-app.config.from_pyfile('config.py')
+app.config.from_pyfile("config.py")
 
 
 @app.before_request
 def pre():
-    '''Before request'''
+    """Before request."""
     g.qbit = _qbc()
 
 
 @app.context_processor
 def injects():
-    '''Variables available to all templates'''
+    """Template variables."""
     return {}
 
 
 def _qbc(
-    host=app.config['QBIT_HOST'], port=app.config['QBIT_PORT'],
-    username=app.config['QBIT_USERNAME'],
-    password=app.config['QBIT_PASSWORD']
+    host=app.config["QBIT_HOST"], port=app.config["QBIT_PORT"],
+    username=app.config["QBIT_USERNAME"],
+    password=app.config["QBIT_PASSWORD"]
 ):
-    '''Connect and log in to qBittorrent API'''
+    """Connect and log in to qBittorrent API"""
     client = qbittorrentapi.Client(
-        host=host, port=port,
-        username=username, password=password,
-        VERIFY_WEBUI_CERTIFICATE=True, FORCE_SCHEME_FROM_HOST=True,
+        host=host,
+        port=port,
+        username=username,
+        password=password,
+        VERIFY_WEBUI_CERTIFICATE=True,
+        FORCE_SCHEME_FROM_HOST=True,
         SIMPLE_RESPONSES=True
     )
     client.auth_log_in()
     return client
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def index(torrents=None):
-    '''List torrents'''
+    """List torrents."""
     if torrents is None:
         torrents = g.qbit.torrents_info()
+
     if not torrents:
-        flash('Sorry, but no torrents could be found!', 'error')
+        flash("Sorry, but no torrents were found!", "error")
         abort(404)
-    return render_template('index.html.j2', torrents=torrents)
+
+    return render_template("index.html.j2", torrents=torrents)
 
 
-@app.route('/category/<path:category>/', methods=['GET'])
-@app.route('/category/<path:category>', methods=['GET'])
+@app.route("/category/<path:category>/", methods=["GET"])
+@app.route("/category/<path:category>", methods=["GET"])
 def search_category(category=None):
-    '''List specifically categorized torrents'''
+    """List categorized torrents."""
     if not category:
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
     return index(torrents=g.qbit.torrents_info(category=category))
 
 
-def error(
-        title='Unknown Error',
-        message='Sorry, but there was an unknown error.',
+def _error(
+        title="Unknown Error",
+        message="Sorry, but there was an unknown error.",
         code=500
 ):
-    '''Error template'''
-    return render_template('error.html.j2', title=title, message=message), code
+    """Error template."""
+    return render_template("error.html.j2", title=title, message=message), code
 
 
 @app.errorhandler(400)
-def bad_request(message):
-    '''Error codes to template above'''
-    return error(title='Bad Request', message=message, code=400)
+def _bad_request(message):
+    """Error codes to template above."""
+    return _error(title="Bad Request", message=message, code=400)
 
 
 @app.errorhandler(404)
-def page_not_found(message):
-    '''Error codes to template above'''
-    return error(title='Page Not Found', message=message, code=404)
+def _page_not_found(message):
+    """Error codes to template above."""
+    return _error(title="Page Not Found", message=message, code=404)
 
 
 @app.errorhandler(500)
-def internal_server_error(message):
-    '''Error codes to template above'''
-    return error(title='Internal Server Error', message=message, code=500)
+def _internal_server_error(message):
+    """Error codes to template above."""
+    return _error(title="Internal Server Error", message=message, code=500)
 
 
-@app.route('/favicon.ico')
-def favicon():
-    '''Serve favicon.ico from static content'''
+@app.route("/favicon.ico")
+def _favicon():
+    """Serve favicon.ico from static content."""
     return send_from_directory(
-        os.path.join(app.root_path, 'static'),
-        'favicon.ico', mimetype='image/vnd.microsoft.icon'
+        os.path.join(app.root_path, "static"),
+        mimetype="image/vnd.microsoft.icon",
+        path="favicon.ico"
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
---
+++ static/tbl.js
@@ -0,0 +1,154 @@
+// Added.
+function fmtAdded (added, type) {
+    const date = new Date(added*1000);
+    const when = date.toLocaleDateString(
+        "en-us", {
+            weekday: "short",
+            year: "numeric",
+            month: "short",
+            day: "numeric",
+            hour: "2-digit",
+            minute: "2-digit"
+        }
+    );
+    if (type == "display") {
+        return `<time class="small" datetime="${date.toISOString()}" title="${date.toString()}">${when}</time>`;
+    };
+    return when;
+};
+
+// Category.
+function fmtCategory (category) {
+    let categoryIcon = "question";
+    if (category == "software") {
+        categoryIcon = "code-slash";
+    } else if (category.startsWith("radarr")) {
+        categoryIcon = "film";
+    } else if (category.startsWith("sonarr")) {
+        categoryIcon = "tv";
+    };
+    return `<a class="bi bi-${categoryIcon}" title="${category}" href="/category/${category}"></a>`;
+};
+
+// Name.
+function fmtName (name) {
+    return `<small title="${name}">${name}</small>`;
+}
+
+// Ratio.
+function fmtRatio (ratio) {
+    let ratioClass = "verylow";
+    if (ratio >= 1) {
+        ratioClass = "done";
+    } else if (ratio >= 0.5) {
+        ratioClass = "high";
+    } else if (ratio >= 0.25) {
+        ratioClass = "low";
+    };
+    const rounded = Number(ratio.toFixed(3));
+    return `<b class="ratio-${ratioClass}" title="${ratio}">${rounded}</b>`;
+};
+
+// State.
+function fmtState (state){
+    let stateClass = "error";
+    let stateIcon = "question";
+    if (state.startsWith("stalled")) {
+        stateIcon = "pause";
+    } else if (state.endsWith("UP")) {
+        stateClass = "success";
+        stateIcon = "arrow-up";
+    } else if (state.endsWith("DOWN")) {
+        stateClass = "info"
+        stateIcon = "arrow-down";
+    };
+    return `<b class="bi bi-${stateIcon}-circle flash-${stateClass}" title="${state}"></b>`;
+};
+
+// Format bytes to human-readable size.
+function fmtSize (bytes, si=false, dp=1) {
+    const thresh = si ? 1000 : 1024;
+
+    if (Math.abs(bytes) < thresh) {
+        return bytes + ' B';
+    }
+
+    const units = si
+        ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+        : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+    let u = -1;
+    const r = 10**dp;
+
+    do {
+        bytes /= thresh;
+        ++u;
+    } while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1);
+
+    const out = bytes.toFixed(dp) + ' ' + units[u];
+    return `<code title="${out}">${out}</code>`;
+};
+
+// Column format definition for DataTables.
+const columns= [
+    {
+        data: "name",
+        render: function (data, type) {
+            return type === "display" ? fmtName(data) : data;
+        },
+        title: "Name",
+        type: "text"
+    },
+    {
+        data: "hash",
+        searchable: false,
+        sortable: false,
+        title: "Hash",
+        type: "text",
+        visible: false
+    },
+    {
+        data: "state",
+        render: function (data, type) {
+            return type === "display" ? fmtState(data) : data;
+        },
+        searchable: false,
+        title: "State",
+        type: "text"
+    },
+    {
+        data: "category",
+        render: function (data, type) {
+            return type === "display" ? fmtCategory(data) : data;
+        },
+        title: "Category",
+        type: "text"
+    },
+    {
+        data: "size",
+        render: function (data, type) {
+            return type === "display" ? fmtSize(data) : data;
+        },
+        title: "Size",
+        type: "num"
+    },
+    {
+        data: "ratio",
+        render: function (data, type, row, meta) {
+            return type === "display" ? fmtRatio(data) : data;
+        },
+        searchable: false,
+        title: "Ratio",
+        type: "num"
+    },
+    {
+        data: "added_on",
+        render: function (data, type) {
+            return type === "display" || type === "filter" ? fmtAdded(data, type) : data;
+        },
+        title: "Added",
+        type: "num"
+    }
+];
+
+const torrents = [];
+function buildTorrent(torrent) { torrents.push(torrent); };
